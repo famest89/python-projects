b57100bfa501cc6d9eb7b30512908994ca2ac667
fernando <famest89@gmail.com>
2025-06-28 15:34:18 +0700

    add dice rolling game

--- dice_rolling_game.py
+++ dice_rolling_game.py
@@ -1,1 +1,23 @@
-print("initial commit")
+######################################################################
+###                       DICE ROLLING GAME                        ###
+######################################################################
+
+import random
+
+
+def main():
+    while True:
+        choice = input("Roll the dice? (y/n): ").lower()
+        if choice == "y":
+            die1 = random.randint(1, 6)
+            die2 = random.randint(1, 6)
+            print(f"({die1}, {die2})")
+        elif choice == "n":
+            print("Thank you")
+            break
+        else:
+            print("Invalid choice!")
+
+
+if __name__ == "__main__":
+    main()
