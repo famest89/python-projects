14ea809e068c624c653b72d9cc7eaf021c8f9736
fernando <famest89@gmail.com>
2025-06-28 15:57:17 +0700

    add number guessing game

---
+++ number_guessing_game.py
@@ -0,0 +1,28 @@
+######################################################################
+###                       NUMBER GUESSING GAME                     ###
+######################################################################
+
+import random
+
+
+def main():
+    number_to_guess = random.randint(1, 100)
+
+    while True:
+        try:
+            guess = int(input("Guess the number between 1 and 100: "))
+
+            if guess > number_to_guess:
+                print("Too high")
+            elif guess < number_to_guess:
+                print("Too low")
+            else:
+                print("Congratulations! You guessed the number.")
+                break
+
+        except ValueError:
+            print("Please enter a valid number")
+
+
+if __name__ == "__main__":
+    main()
